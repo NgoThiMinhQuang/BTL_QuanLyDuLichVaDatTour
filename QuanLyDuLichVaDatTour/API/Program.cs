00f4953b866e62c9b1850048125747ff72f21a78
NgoThiMinhQuang <minhquang17012005@gmail.com>
2026-03-27 23:35:36 +0700

    noi API voi FE

--- QuanLyDuLichVaDatTour/API/Program.cs
+++ QuanLyDuLichVaDatTour/API/Program.cs
@@ -30,6 +30,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("FrontendPolicy", policy =>
     {
         policy.WithOrigins(
+                "http://localhost:5175",
+                "https://localhost:5175",
                 "http://localhost:5174",
                 "https://localhost:5174",
                 "http://localhost:5173",
