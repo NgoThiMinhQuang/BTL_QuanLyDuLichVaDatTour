386c2c6c6a2ac6bf22a7aedc1f99526d4c9f9c90
NgoThiMinhQuang <minhquang17012005@gmail.com>
2026-04-04 08:28:31 +0700

    sua API tour da dat

--- BE_QuanLyDuLichVaDatTour/BE_QuanLyDuLichVaDatTour/Data/AppDbContext.cs
+++ BE_QuanLyDuLichVaDatTour/BE_QuanLyDuLichVaDatTour/Data/AppDbContext.cs
@@ -136,6 +136,75 @@ public class AppDbContext : DbContext
                 .HasDatabaseName("IdxDiaDiem_TinhThanh_QuocGia");
         });
 
+        modelBuilder.Entity<NguoiDung>(entity =>
+        {
+            entity.ToTable("NguoiDung");
+
+            entity.HasKey(x => x.Id);
+
+            entity.Property(x => x.Id)
+                .HasColumnName("NguoiDungId")
+                .HasColumnType("bigint")
+                .ValueGeneratedOnAdd();
+
+            entity.Property(x => x.Email)
+                .HasColumnName("Email")
+                .HasMaxLength(255)
+                .IsRequired();
+
+            entity.Property(x => x.MatKhau)
+                .HasColumnName("MatKhau")
+                .HasMaxLength(255)
+                .IsRequired();
+
+            entity.Property(x => x.HoTen)
+                .HasColumnName("HoTen")
+                .HasMaxLength(200)
+                .IsRequired();
+
+            entity.Property(x => x.SoDienThoai)
+                .HasColumnName("SoDienThoai")
+                .HasMaxLength(20);
+
+            entity.Property(x => x.DiaChi)
+                .HasColumnName("DiaChi")
+                .HasMaxLength(300);
+
+            entity.Property(x => x.AnhDaiDien)
+                .HasColumnName("AnhDaiDien")
+                .HasMaxLength(500);
+
+            entity.Property(x => x.VaiTro)
+                .HasColumnName("VaiTro")
+                .HasConversion<string>()
+                .HasMaxLength(50)
+                .IsRequired();
+
+            entity.Property(x => x.TrangThai)
+                .HasColumnName("TrangThai")
+                .HasConversion<string>()
+                .HasMaxLength(50)
+                .IsRequired();
+
+            entity.Property(x => x.CreatedAt)
+                .HasColumnName("CreatedAt")
+                .HasColumnType("datetime2")
+                .HasDefaultValueSql("SYSDATETIME()")
+                .ValueGeneratedOnAdd();
+
+            entity.Property(x => x.UpdatedAt)
+                .HasColumnName("UpdatedAt")
+                .HasColumnType("datetime2")
+                .HasDefaultValueSql("SYSDATETIME()")
+                .ValueGeneratedOnAdd();
+
+            entity.HasIndex(x => x.Email)
+                .IsUnique();
+
+            entity.HasIndex(x => new { x.VaiTro, x.TrangThai })
+                .HasDatabaseName("IdxNguoiDung_VaiTro_TrangThai");
+        });
+
         modelBuilder.Entity<Tour>(entity =>
         {
             entity.ToTable("Tour");
